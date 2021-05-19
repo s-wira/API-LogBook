53f3f4e57d82aa30a58ff3b8c5bbf0757801f567
madrismawan <rismawannugraha10@gmail.com>
2021-05-20 01:15:45 +0800

    "Post User"

--- src/main/java/com/dimata/logbookAPI/service/AppUserService.java
+++ src/main/java/com/dimata/logbookAPI/service/AppUserService.java
@@ -2,6 +2,7 @@ package com.dimata.logbookAPI.service;
 
 import com.dimata.logbookAPI.model.AppUser;
 import com.dimata.logbookAPI.repository.AppUserRepo;
+import com.dimata.logbookAPI.utility.GenerateOID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +15,10 @@ public class AppUserService {
     @Autowired
     private AppUserRepo appUserRepo;
 
+//    inget harus sesuai dengan penulisan code seberapa dia panjanga charcternya
     public AppUser create(AppUser appUser){
+        appUser.setUserId(GenerateOID.generateOID());
+        appUser.setEmployeeId(123L);
         return appUserRepo.save(appUser);
     }
 
