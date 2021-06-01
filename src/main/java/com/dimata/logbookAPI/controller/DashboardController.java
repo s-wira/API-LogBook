c1e12b30d312c9451588362e8df3197bbd0a485f
madrismawan <rismawannugraha10@gmail.com>
2021-06-01 16:33:49 +0800

    "Get Dashboard Testing"

--- src/main/java/com/dimata/logbookAPI/controller/DashboardController.java
+++ src/main/java/com/dimata/logbookAPI/controller/DashboardController.java
@@ -1,5 +1,6 @@
 package com.dimata.logbookAPI.controller;
 
+import com.dimata.logbookAPI.model.LogReport;
 import com.dimata.logbookAPI.service.LogReportService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,18 +8,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/dashboard")
 public class DashboardController {
 
     @Autowired
-    LogReportService logReportService;
+    private LogReportService logReportService;
 
 //    @GetMapping("/{id}")
-//    public Integer countInProgres(@PathVariable("id")Integer id){
+//    public Long countInProgres(@PathVariable("id")Integer id){
 //        return logReportService.countStatus(id);
 //    }
 
+    @GetMapping("/status/{id}")
+    public List<LogReport> findByStatus (@PathVariable("id") Integer status){
+        return logReportService.findAllStatus(status);
+    }
 
 
 }
